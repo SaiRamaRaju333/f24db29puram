79e22988eb152074a6cfd655ede2b71802b10bb8
Sai Rama Raju Puram <sairamrajpuram2002@gmail.com>
2024-11-15 02:18:31 -0600

    Update toy.js

--- controllers/toy.js
+++ controllers/toy.js
@@ -1,21 +1,33 @@
 var Toy = require('../models/toy');
-// List of all toys
-exports.toy_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Toy list');
-};
-// for a specific toy.
+// List of all Costumes
+// exports.toy_list = function(req, res) {
+// res.send('NOT IMPLEMENTED: Toy list');
+// };
+// List of all Costumes
+exports.toy_list = async function(req, res) {
+    try{
+    theToy = await Toy.find();
+    res.send(theToy);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+   
+// for a specific Costume.
 exports.toy_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Toy detail: ' + req.params.id);
 };
-// Handle toy create on POST.
+// Handle Costume create on POST.
 exports.toy_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: Toy create POST');
 };
-// Handle toy delete from on DELETE.
+// Handle Costume delete from on DELETE.
 exports.toy_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Toy delete DELETE ' + req.params.id);
 };
-// Handle toy update form on PUT.
+// Handle Costume update form on PUT.
 exports.toy_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Toy update PUT' + req.params.id);
 };
